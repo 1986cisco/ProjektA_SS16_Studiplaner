622b66e15939823167a85269341f3e8dcefa60b4
1986cisco <cisco-sv@gmx.de>
2016-11-02 22:24:50 +0100

    Create Punktetabelle.php

---
+++ Gruppen_de/Punktetabelle.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+	<meta charset="utf-8">
+	<title>Hauptspielbildschirm</title>
+</head>
+	
+<body>
+
+<!-- Tabelle Punkteübersicht aller Spieler -->
+<fieldset>
+		<table>
+			<caption>Punkte</caption>
+			<tbody>
+				<tr>
+					<th>Spielername</th>
+					<th>Pkt.</th>
+				</tr>
+			</tbody>
+		</table>
+	</fieldset>
+
+</body>
+
+</html>
