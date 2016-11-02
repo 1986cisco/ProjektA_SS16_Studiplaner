f4963d632c1fab7429f2fb2641af5a20ff0676e4
1986cisco <cisco-sv@gmx.de>
2016-11-02 22:25:58 +0100

    Create Finanztabelle.php

---
+++ Gruppen_de/Finanztabelle.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+	<meta charset="utf-8">
+	<title>Hauptspielbildschirm</title>
+</head>
+	
+<body>
+
+<!-- Tabelle Finanzübersicht -->
+	<fieldset>
+		<table>
+		<caption>Finanzen</caption>
+			<tbody>
+				<tr>
+					<th>Kontostand</th>
+					<th>Einnahmen</th>
+					<th>Ausgaben</th>
+					<th>Gewinn</th>
+				</tr>
+			</tbody>
+		</table>
+	</fieldset>
+  
+</body>
+
+</html>
