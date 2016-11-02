61b610253e3c1d6ceced4289f1cbba70219e85ed
1986cisco <cisco-sv@gmx.de>
2016-11-02 22:28:46 +0100

    Create Rundeninfo.php

---
+++ Gruppen_de/Rundeninfo.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+	<meta charset="utf-8">
+	<title>Hauptspielbildschirm</title>
+</head>
+	
+<body>
+
+<!-- Rundeninfo -->
+	<fieldset>
+	<p>Spieler {echo} hat {echo} erforscht</p>
+	<p>Spieler {echo} ist am Zug</p>
+	<p>Runde {echo}/{echo}</p>
+	</fieldset>
+  
+</body>
+
+</html>
