ad87edd67f726536827e4e805f704515be5530d0
1986cisco <cisco-sv@gmx.de>
2016-11-02 22:27:17 +0100

    Create Aktionsleiste.php

---
+++ Gruppen_de/Aktionsleiste.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+	<meta charset="utf-8">
+	<title>Hauptspielbildschirm</title>
+</head>
+	
+<body>
+<!-- Führt zu den Hauptspielaktionsmöglichkeiten -->
+	<fieldset>
+		<input type=button value="Forschung">
+		<input type=button value="Produktion">
+		<input type=button value="Rohstoffmarkt">
+		<input type=button value="Schwarzmarkt">
+		<input type=button value="Spielerhandel">
+	</fieldset>
+</body>
+
+</html>
