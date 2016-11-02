1d5f9eeb66650e45ccde036424153dc1a62a314b
1986cisco <cisco-sv@gmx.de>
2016-11-02 22:21:51 +0100

    Create Hauptleiste.php

---
+++ Gruppen_de/Hauptleiste.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+	<meta charset="utf-8">
+	<title>Hauptspielbildschirm</title>
+</head>
+	
+<body>
+	
+	<!-- Über diese Buttons kann man das Spiel speichern, zurück zum Startmemnü kommen, sich ausloggen und die Runde beenden -->
+	<fieldset>
+		<input type=button value="Speichern">
+		<input type=button value="Startmenue">
+		<input type=button value="Logout">
+		<input type=button value="Runde Beenden">
+	</fieldset>
+	
+</body>
+
+</html>
